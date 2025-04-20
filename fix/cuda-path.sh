a6013a5d089059b5e9d5f9bf6742e01bf3443fa1
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2025-04-20 19:04:40 +0700

    export only works in the scripts subsession - fixed with source

--- fix/cuda-path.sh
+++ fix/cuda-path.sh
@@ -27,8 +27,7 @@ add_line_if_missing "$LINE1" "$BASHRC"
 add_line_if_missing "$LINE2" "$BASHRC"
 
 # Apply the changes to the current shell session
-export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+source ~/.bashrc
 
 # Inform the user
 echo "Changes applied! Both the ~/.bashrc file and the current shell session have been updated."
